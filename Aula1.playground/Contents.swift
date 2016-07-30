9d519b99ed0931b9578a081b235eb3403f57886b
Douglas Queiroz <douglasq16@gmail.com>
2016-07-30 15:48:16 -0300

    Firts commit

---
+++ Aula1.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var nome = "Douglas"
+var numero: Float = 12.5
+var doubleNum: Double
+
+var dic = [String: String]()
+// Add and Update
+dic["Douglas"] = "teste"
+// Remove
+dic.removeValueForKey("Douglas")
+
+// Init
+var dicInit = ["Douglas": "Tes"]
+
+var arr = [String]()
+let arrInt = ["Douglas", "Alex", "Luiz"]
